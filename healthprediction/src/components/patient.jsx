9ebc700e5951413817d0504df3ca1ed6544aabf6
jeevanthask <jeevanthanandika@gmail.com>
2019-11-30 20:45:02 +0530

    patient page designed

--- healthprediction/src/components/patient.jsx
+++ healthprediction/src/components/patient.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../styles/patientstyle.css'
 
 class Patient extends React.Component{
 
@@ -25,13 +26,24 @@ class Patient extends React.Component{
 
     render() {
         return (
-            <form onSubmit={this.handleSubmit}>
-                <label>
-                    Name:
-                    <input type="text" value={this.state.value} onChange={this.handleChange} />
-                </label>
-                <input type="submit" value="Submit" />
-            </form>
+            <div className="container m-5 bg-light p-5">
+                <form onSubmit={this.handleSubmit}>
+
+                    <div className="form-group row">
+                        <label htmlFor="inputPassword" className="col-md-6 col-form-label">Please enter a symptom (any one symptom,leave no any blank spaces before or after of it )</label>
+                        <div className="col-md-6">
+                            <input type="text" className="form-control" value={this.state.value} onChange={this.handleChange}/>
+                        </div>
+
+                        <div className="col-md-12 ">
+                            <input type="submit" className="btn btn-danger" value="Next"/>
+                        </div>
+                    </div>
+
+                </form>
+
+            </div>
+
         );
     }
 }
