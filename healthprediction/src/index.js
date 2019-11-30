7d3c33808cc4c74c74c6da1fa8e05d0833350e1f
jeevanthask <jeevanthanandika@gmail.com>
2019-11-30 16:13:29 +0530

    patient page updated

--- healthprediction/src/index.js
+++ healthprediction/src/index.js
@@ -2,14 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
 import App from "./app";
-import App2 from "./app2"
-
-
+//import App2 from "./app2"
 
 ReactDOM.render(
     <div>
         <App/>
-        <App2 />
     </div>,
     document.getElementById('root'));
 
